d346b2f00cda74855da94512ffa04882688570b0
Jlanglang <347891332@qq.com>
2020-05-11 00:48:18 +0800

    1.更新

--- myapplication/src/main/java/com/baozi/myapplication/App.java
+++ myapplication/src/main/java/com/baozi/myapplication/App.java
@@ -8,7 +8,6 @@ import com.baozi.linfeng.NetWorkManager;
 import com.baozi.linfeng.factory.JSONFactory;
 import com.baozi.linfeng.location.APICallBack;
 import com.baozi.linfeng.location.onExceptionListener;
-import com.baozi.linfeng.location.retrofit.ParseInfo;
 import com.google.gson.JsonElement;
 
 import java.net.SocketException;
@@ -18,12 +17,8 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         NetWorkManager.init("https://api.apiopen.top/", this);
-        NetWorkManager.initKey("私钥", "公钥");//加密解密
         NetWorkManager.setDefaultRetry(5);//重试次数
         NetWorkManager.setDefaultTimeOut(20);//秒
-        NetWorkManager.addParseInfo(
-                new ParseInfo("code", "result", "message", "200")
-        );
         NetWorkManager.setExceptionListener(new onExceptionListener() {
             @Override
             public String onError(Throwable throwable) {
